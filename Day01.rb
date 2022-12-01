04187d38c1a8d88098f14174f6cbba5f037b7b36
goesting <75796795+goesting@users.noreply.github.com>
2022-12-01 15:53:53 +0100

    Update Day01.rb
    Replaced the code from the webversion with vscode attempt. Code for reading/validating inputs was added

--- Day01.rb
+++ Day01.rb
@@ -1,7 +1,21 @@
-#input is just the entire txt file, hardcoded in the code i put in the web version
-
+### Settings ###
+doingTest = false
+day=1
+### Load file ###
+filePointer = doingTest ? "C:\\Users\\tpz1ppc\\AoC22\\input\\day#{day.to_s.rjust(2,'0')}test.txt" 
+                        : "C:\\Users\\tpz1ppc\\AoC22\\input\\day#{day.to_s.rjust(2,'0')}.txt"
+if !File.exist?(filePointer)
+    warn "You forgot the input, dummy. @ #{filePointer}"
+elsif File.zero?(filePointer)
+    warn "Input file is empty."
+else
+    input = File.open(filePointer, "r").map{_1}*''
+################
+### Solution ###
+################
 #part 1
 puts input.split("\n\n").map{_1.split.map(&:to_i).sum}.max
 
 #part 2
 puts input.split("\n\n").map{_1.split.map(&:to_i).sum}.sort[-3..].sum
+end
