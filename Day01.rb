8aee9167027bdec90d4ccd0c34bdce17f98ff00d
goesting <75796795+goesting@users.noreply.github.com>
2022-12-01 15:39:57 +0100

    Update Day01.rb
    corrected typo in comments

--- Day01.rb
+++ Day01.rb
@@ -1,4 +1,4 @@
-#inputs is just the entire txt file
+#input is just the entire txt file, hardcoded in the code i put in the web version
 
 #part 1
 puts input.split("\n\n").map{_1.split.map(&:to_i).sum}.max
