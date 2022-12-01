300a3438f0c1cfd5af067473bd14e64da2885112
goesting <75796795+goesting@users.noreply.github.com>
2022-12-01 16:17:10 +0100

    Update Day01_short.rb
    removed the need for double \\ by using ' instead of "

--- Day01_short.rb
+++ Day01_short.rb
@@ -1,2 +1,2 @@
-input = File.read("C:\\Users\\tpz1ppc\\AoC22\\input\\day01.txt").split("\n\n").map{_1.split.map(&:to_i).sum}
+input = File.read('C:\Users\tpz1ppc\AoC22\input\day01.txt').split("\n\n").map{_1.split.map(&:to_i).sum}
 puts input.max,input.sort[-3..].sum
