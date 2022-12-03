e2bf22151ba8ffd96141cbe48801bd78818c2da3
goesting <75796795+goesting@users.noreply.github.com>
2022-12-03 15:40:24 +0100

    Create Day03.rb

---
+++ Day03.rb
@@ -0,0 +1,6 @@
+f = File.read('Day03.txt').split
+#part 1
+puts f.map{|x| [x[...x.size/2],x[x.size/2..]]}.map{|x| x[0].chars & x[1].chars}.flatten.map{_1.ord - 96}.map{_1 < 0 ? _1+58 : _1}.sum
+
+#part 2
+puts f.each_slice(3).to_a.map{|x| x.map(&:chars).reduce(:&)}.flatten.map{_1.ord - 96}.map{_1 < 0 ? _1+58 : _1}.sum
